830fc1718eacd1c77fdaff67e5eea581dbbc8842
ASHIVANI62 <87575397+ASHIVANI62@users.noreply.github.com>
2021-07-19 18:45:58 -0700

    Delete count no of bits.C

--- count no of bits.C
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int number;
-	printf("Enter a number:\n");
-	scanf("%d",&number);
-	printf("number of bits in %d is %d",number,sizeof(number)*8);	
-	return 0;
-}
