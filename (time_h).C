f482fd145dc067e73848f1d2c8a9124d8c9fbaa3
ASHIVANI62 <87575397+ASHIVANI62@users.noreply.github.com>
2021-08-19 00:44:54 -0700

    Add files via upload

---
+++ (time_h).C
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	long double d;
+	d=5.6;
+    //srand(time(0));
+    //printf("%d ", rand());
+    printf("%lg ", d);
+    return 0;
+}
+
