080b63113b7cc735551c121b9ad020ab15fc4e66
ASHIVANI62 <87575397+ASHIVANI62@users.noreply.github.com>
2021-08-19 05:52:33 -0700

    Add files via upload

---
+++ Array_1D_Fun_Ref.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void printArray(int *,int);
+int main()
+{
+	int A[10],i,n; 
+	printf("\nEnter n");
+	scanf("%d",&n); 
+	printf("Enter %d numbers",n);
+	 for(i=0;i<n;i++)
+	   scanf("%d",&A[i]);
+	 printf("\n");                                
+	printArray(A,n);
+	printf("\n");//Address 
+	printArray(A,n);
+	printf("\n");
+	printArray(A,n);
+	
+	
+	return 1;
+}
+void printArray(int *ptr,int n)
+ {
+  
+int i;
+  for ( i = 0; i < n; ++i) {
+   printf("%5d",*(ptr+i));
+  }
+
+
+}
