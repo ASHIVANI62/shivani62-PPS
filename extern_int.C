00ce7d4b1e12418237a8fb2df4a048f868fac29d
ASHIVANI62 <87575397+ASHIVANI62@users.noreply.github.com>
2021-08-24 08:01:36 -0700

    Add files via upload

---
+++ extern_int.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+extern int i;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	printf("%d",i);
+	return 0;
+}
