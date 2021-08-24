e819054bf0d48916a9a4115288ef46dc4214484d
ASHIVANI62 <87575397+ASHIVANI62@users.noreply.github.com>
2021-08-24 06:53:54 -0700

    Add files via upload

---
+++ datatypes.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	char ans;
+	int num=97;
+	float realnum=4.5;
+	double dnum=6.7;
+	printf("\nEnter a character :");
+	scanf("%c",&ans);
+	printf("The value of ans is = %d",ans);
+	printf(" \nThe value of num =  %c",num);
+	printf(" \nthe value of realnum= %d %d",realnum,dnum);
+}
